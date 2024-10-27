8eebe149da8808ac5494e7ca2a7b4bcb0deed188
ahyanKamal070705 <ahyankamal070705@gmail.com>
2024-10-27 05:35:24 +0530

    completed nahi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const changHandler = (e) => {
+    const { name, value, checked, type } = e.target;
+
+    setForm((prev) => {
+      return {
+        ...prev,
+        [name]: type === "checkbox" ? checked : value,
+      };
+    });
+  };
+
+  const [formData, setForm] = useState({
+    firstName: "",
+    lastName: "",
+    visible: false,
+    mode: "",
+    favCar:"",
+  });
+
+ 
+
+  function submitHandler(e){
+     e.preventDefault();
+     console.log(formData);
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          placeholder="first name"
+          onChange={changHandler}
+          value={formData.firstName}
+          name="firstName"
+        />
+
+        <input
+          type="text"
+          placeholder="last name"
+          onChange={changHandler}
+          value={formData.lastName}
+          name="lastName"
+        />
+
+        <input
+          type="checkBox"
+          name="visible"
+          id="visible"
+          onChange={changHandler}
+          checked={formData.visible}
+        />
+        <label htmlFor="visible">am i visible</label>
+
+        <fieldset>
+
+          <legend>Mode:</legend>
+
+          {/* when making radio button how many u make make the name same */}
+          <input
+            type="radio"
+            name="mode"
+            value="online"
+            id="online"
+            onChange={changHandler}
+            checked={formData.mode === "online"}
+          />
+          <label htmlFor="online">online</label>
+
+          <input
+            type="radio"
+            name="mode"
+            value="offline"
+            id="offline"
+            onChange={changHandler}
+            checked={formData.mode === "offline"}
+          />
+
+          <label htmlFor="offline">offline</label>
+
+
+        </fieldset>
+
+         <select name="favCar" value={formData.favCar}  onChange={changHandler}>
+           
+         <option value="thar">Select</option>
+          <option value="scorpipo">scorpio</option>
+          <option value="fortuner">fortuner</option>
+          <option value="thar">thar</option>
+           
+         </select>
+         <button>Submit</button>   
+      </form>
     </div>
   );
 }
